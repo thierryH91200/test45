247c5f67833ada6117c422d24b179c9369e5a36d
thierry <thierry.hentic@gmail.com>
2025-03-27 10:50:22 +0100

    Initial Commit

---
+++ test45/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test45
+//
+//  Created by Thierry hentic on 27/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test45/test45App.swift
@@ -0,0 +1,17 @@
+//
+//  test45App.swift
+//  test45
+//
+//  Created by Thierry hentic on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct test45App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
